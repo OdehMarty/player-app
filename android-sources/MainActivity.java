b39a7f4557a93af6ec7d760f99b681560216aa01
OdehMarty <odehmartinluther7@gmail.com>
2026-06-15 02:33:37 +0100

    Create MainActivity.java

---
+++ android-sources/MainActivity.java
@@ -0,0 +1,21 @@
+package com.boxplayer;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import com.boxplayer.plugins.AllFilesAccessPlugin;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(AllFilesAccessPlugin.class);
+        super.onCreate(savedInstanceState);
+        WebView.setWebContentsDebuggingEnabled(true);
+        WebView webView = getBridge().getWebView();
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
+        webSettings.setMediaPlaybackRequiresUserGesture(false);
+    }
+}
